201d61165acd21c449c4a461d5babc33b8c65914
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-04-25 09:01:55 -0300

    Resolvido exercicios do learnyounode: http assync e time server. Ignore para a pasta de modulos do nodejs

--- learnyounode/assyncHttp.js
+++ learnyounode/assyncHttp.js
@@ -1,34 +1,23 @@
 var bl = require('bl');
 var http = require('http');
-var str = [];
-var a;
-var much = 0;
-data = http.get(process.argv[2], function callback(response) {
-    response.pipe(bl(function (error, data) {
-        if(error){
-            console.log('error code:' + error);
-        }
-        str[0] = data.toString();
-        much++;
-    }));
-});
-data.setEncoding('utf-8')
-console.log(data.response);
-// http.get(process.argv[3], function callback(response) {
-//     response.pipe(bl(function (error, data) {
-//         if(error){
-//             console.log('error code:' + error);
-//         }
-//         str[1] = data.toString();
-//         much++;
-//     }));
-// });
-// http.get(process.argv[4], function callback(response) {
-//     response.pipe(bl(function (error, data) {
-//         if(error){
-//             console.log('error code:' + error);
-//         }
-//         str[2] = data.toString();
-//         much++;
-//     }));
-// });
+cont = 0;
+str = [];
+function getContent(where, who){
+    data = http.get(where, function callback(response) {
+        response.pipe(bl(function (error, data) {
+            if(error){
+                console.log('error code:' + error);
+            }
+            str[who] = data.toString();
+            cont++;
+            if (cont == 3){
+                for( d = 0 ; d < 3 ; d++){
+                    console.log(str[d]);
+                }
+            }
+        }));
+    });
+}
+for( a = 0 ; a < 3 ; a++){
+    getContent(process.argv[a + 2], a);
+}
