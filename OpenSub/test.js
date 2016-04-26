c2785ea4631498d6f5dc6086e5d725722b4715d1
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-04-26 20:16:05 -0300

    test.js funcionando com login, pesquisa e logout(api OpenSubtitles)

--- OpenSub/test.js
+++ OpenSub/test.js
@@ -1,17 +1,32 @@
 var OS = require('opensubtitles-api');
 var OSub = new OS({
-    useragent:'UserAgent',
-    username: 'Username',
-    password: 'Password',
-    ssl: false
+    useragent:'OSTestUserAgent', //test useragent
+    username: 'user',
+    password: require('crypto').createHash('md5').update('password').digest('hex'),
+    ssl: true
 });
+var chave = '';
 OSub.login()
     .then(function(token){
-        console.log(token);
+        chave = token;
+        OSub.search({
+            sublanguageid: 'pob',
+            path: './silicon/Silicon Valley - s02e01.mp4',
+            filename: 'Silicon Valley - s02e01.mp4',
+            extensions: ['srt', 'vtt'],
+            limit: 'all',
+        }).then(function (subtitles) {
+            console.log(subtitles);
+            OSub.api.LogOut(chave);
+
+        });
+        // console.log(token);
     })
     .catch(function(err){
         console.log(err);
 });
+// s0osnauugg15b18qqd4cc5ak32
+
 // var movie = [];
 // OpenSubtitles.extractInfo('path/to/file.mp4').then(function (infos) {
 //         console.log(infos);
